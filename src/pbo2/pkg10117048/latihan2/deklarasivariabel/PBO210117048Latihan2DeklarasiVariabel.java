378e5c9cc7bd284cc12df88dab1969dd23e3d299
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:31:14 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan2/deklarasivariabel/PBO210117048Latihan2DeklarasiVariabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program untuk menampilkan data 
+ * deklarasi variabel.
+ */
+public class PBO210117048Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
